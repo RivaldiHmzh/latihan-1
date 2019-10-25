a9f339e783b470820f8f66edccf853786e42d850
RivaldiHmzh <56198396+RivaldiHmzh@users.noreply.github.com>
2019-10-25 17:23:54 +0700

    Add files via upload

---
+++ Tugas 1.py
@@ -0,0 +1,14 @@
+namalengkap = "Rivaldi Hamzah"
+namapanggilan = "valdi"
+npm = "311910656"
+tempatlahir = "Bekasi"
+umur = "19"
+telp = "0895-3301-72596"
+alamat = "Cikarang timur"
+
+print("assalamualaikum,\n"
+      "let me introduce my self, my name is",namalengkap,
+      "but you can call me",namapanggilan,
+      ", my npm is",npm,
+      ", i was born in",tempatlahir,
+      "and i am",umur,"years old. i am very glad if you want to invite my house in",alamat,", so don't forget to call me before wit the number",telp,"\n thank you")
